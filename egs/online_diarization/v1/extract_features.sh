03cb8f38751473bbbc7a9791aed5e21de3e2b30b
carlosrcs9 <carloscastillomvc@gmail.com>
2020-02-23 20:57:34 -0500

    callhome_diarization undo

--- egs/online_diarization/v1/extract_features.sh
+++ egs/online_diarization/v1/extract_features.sh
@@ -13,6 +13,7 @@ outputdir=$2
 
 # Prepare features
 if [ $stage -le 1 ]; then
+  rm -rf $outputdir/make_mfcc
   steps/make_mfcc.sh --mfcc-config conf/mfcc_ivectors.conf --nj 40 --cmd "$train_cmd" --write-utt2num-frames true --write-utt2dur true \
     $datadir \
     $outputdir/make_mfcc \
@@ -22,10 +23,14 @@ fi
 
 # Extract i-vectors
 if [ $stage -le 2 ]; then
+  rm -rf $outputdir/make_ivectors
   diarization/extract_ivectors.sh --cmd "$train_cmd --mem 20G" --nj 40 --window 1.5 --period 0.75 --apply-cmn false --min-segment 0.5 \
     exp/extractor_c${num_components}_i${ivector_dim} \
     $datadir \
     $outputdir/make_ivectors
+fi
+
+if [ $stage -le 3 ]; then
   copy-vector \
     scp:$outputdir/make_ivectors/ivector.scp \
     ark,t:$outputdir/make_ivectors/ivector.txt
@@ -33,7 +38,8 @@ if [ $stage -le 2 ]; then
 fi
 
 # Prepare features
-if [ $stage -le 3 ]; then
+if [ $stage -le 4 ]; then
+  rm -rf $outputdir/make_mfcc
   steps/make_mfcc.sh --mfcc-config conf/mfcc_xvectors.conf --nj 40 --cmd "$train_cmd" --write-utt2num-frames true --write-utt2dur true \
     $datadir \
     $outputdir/make_mfcc \
@@ -42,11 +48,25 @@ if [ $stage -le 3 ]; then
 fi
 
 # Extract x-vectors
-if [ $stage -le 4 ]; then
+if [ $stage -le 5 ]; then
+  rm -rf $outputdir/make_xvectors
   diarization/nnet3/xvector/extract_xvectors.sh --cmd "$train_cmd --mem 20G" --nj 40 --window 1.5 --period 0.75 --apply-cmn false --min-segment 0.5 \
     exp/xvector_nnet_1a \
     $datadir \
     $outputdir/make_xvectors
+fi
+
+#if [ $stage -le 6 ]; then
+#  "$train_cmd" $outputdir/make_xvectors/log/plda.log \
+#    ivector-compute-plda ark:$outputdir/make_xvectors/spk2utt \
+#      "ark:ivector-subtract-global-mean \
+#      scp:$outputdir/make_xvectors/xvector.scp ark:- \
+#      | transform-vec $outputdir/make_xvectors/transform.mat ark:- ark:- \
+#      | ivector-normalize-length ark:- ark:- |" \
+#    $outputdir/make_xvectors/plda || exit 1;
+#fi
+
+if [ $stage -le 7 ]; then
   copy-vector \
     scp:$outputdir/make_xvectors/xvector.scp \
     ark,t:$outputdir/make_xvectors/xvector.txt
