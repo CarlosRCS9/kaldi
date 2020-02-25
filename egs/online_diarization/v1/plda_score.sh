d87ba1ad3cbf8c330b1cfc8260853b72d0417147
carlosrcs9 <carloscastillomvc@gmail.com>
2020-02-25 01:30:22 -0600

    TODO: callhome2 ivector plda comparsion

--- egs/online_diarization/v1/plda_score.sh
+++ egs/online_diarization/v1/plda_score.sh
@@ -7,12 +7,16 @@ set -e
 stage=0
 
 if [ $stage -le 0 ]; then
-  rm -rf exp/plda_scores
-  ivector-plda-scoring \
+  echo reference $1 > exp/reference.1.ark
+  echo test $2 > exp/test.1.ark
+fi
+
+if [ $stage -le 1 ]; then
+  ivector-plda-scoring --print-args=false \
     exp/plda \
-    ark:$1 \
-    ark:$2 \
-    $3 \
+    ark:exp/reference.1.ark \
+    ark:exp/test.1.ark \
+    exp/trials \
     exp/plda_scores
   cat exp/plda_scores
 fi
