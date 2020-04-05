1575ecdd487c1c3c67fe9bcd3957a0861199a846
carlosrcs9 <carloscastillomvc@gmail.com>
2020-04-05 16:45:36 -0500

    Cobined audio generation

--- egs/online_diarization/v1/scripts/json_augment_audio.py
+++ egs/online_diarization/v1/scripts/json_augment_audio.py
@@ -10,6 +10,7 @@ import subprocess
 import sys
 import json
 import itertools
+import math
 
 from models import Segment_complex
 
@@ -113,7 +114,7 @@ def main():
       timestamps = sorted([(round(segment.begining, 2), round(segment.duration, 2)) for segment in speaker_segments], key = lambda tuple: tuple[0])
       filepath = args.output_folder + recording_id + '_' + speaker_id + '.' + recording_extension
       filepath, duration = sox_stich_audio(scp[recording_id], timestamps, filepath)
-      speakers_stiched[speaker_id] = { 'filepath': filepath, 'duration': duration }
+      speakers_stiched[speaker_id] = { 'filepath': filepath, 'duration': math.floor(duration * 100)/100.0 }
     for combination in [sorted(combination) for combination in list(itertools.combinations([speaker_id for speaker_id in speakers_stiched], 2))]:
       filepaths = [speakers_stiched[speaker_id]['filepath'] for speaker_id in combination]
       durations = [speakers_stiched[speaker_id]['duration'] for speaker_id in combination]
