5b5089c7ce3f12d2aadff9e7185346c87688cb26
carlosrcs9 <carloscastillomvc@gmail.com>
2020-04-06 15:14:11 -0500

    TODO: insert new segments

--- egs/online_diarization/v1/scripts/json_augment_audio.py
+++ egs/online_diarization/v1/scripts/json_augment_audio.py
@@ -148,6 +148,7 @@ def main():
       combinations_segments_mix.append(combinations_segments[combination_index].pop(0))
       combinations_segments_lengths = [len(combination_segments) for combination_segments in combinations_segments]
 
+    print(recording_id)
     print(combinations_segments_mix)
     
     '''recording_segments_index = 0
