fc0fd5be64a8f568c4c97de4750972d8fcd100cb
carlosrcs9 <carloscastillomvc@gmail.com>
2020-04-06 14:37:25 -0500

    TODO: insert new segments

--- egs/online_diarization/v1/scripts/json_augment_audio.py
+++ egs/online_diarization/v1/scripts/json_augment_audio.py
@@ -159,7 +159,7 @@ def main():
         begining = option['timestamp'][0]
         duration = option['timestamp'][1]
         speakers_ids = option['combination']
-        last_ending = new_recording_segments[-1].ending len(new_recording_segments) > 0 else 0
+        last_ending = new_recording_segments[-1].ending if len(new_recording_segments) > 0 else 0
         print('last_ending', last_ending)
       options_lengths = [len(option) for option in options]
     print(new_recording_segments)
