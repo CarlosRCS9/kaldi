66a1b562b058cd369047cd3cd599d4aa58b540c4
carlosrcs9 <carloscastillomvc@gmail.com>
2020-04-05 23:20:24 -0500

    TODO: mix with original audio

--- egs/online_diarization/v1/scripts/json_augment_audio.py
+++ egs/online_diarization/v1/scripts/json_augment_audio.py
@@ -97,7 +97,7 @@ def main():
   recordings_segments = reduce(get_recordings_segments, segments, {})
   for recording_id in sorted(list(recordings_segments.keys())):
     recording_extension = scp[recording_id].split('.')[-1]
-    recording_segments = recordings_segments[recording_id]
+    recording_segments = sorted(recordings_segments[recording_id], key = lambda segment: segment.begining)
     speakers_segments = reduce(lambda acc, segment: get_speakers_segments(acc, segment, ['A', 'B']), recording_segments, {})
     speakers_stiched = {}
     for speaker_id in speakers_segments:
