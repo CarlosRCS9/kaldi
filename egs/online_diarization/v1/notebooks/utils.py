3d9da91c6e4dd0fda7bf29d96e8acb90dcc1157b
carlosrcs9 <carloscastillomvc@gmail.com>
2020-04-26 12:54:37 -0500

    min_dcf wrapper

---
+++ egs/online_diarization/v1/notebooks/utils.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+# Copyright 2019 Carlos Castillo
+# Apache 2.0.
+
+import subprocess
+
+def min_dcf(scores_filepath, trials_filepath, target_probability = 0.01, log_directory = None):
+  bin = '../sid/compute_min_dcf.py'
+  command = [bin, '--p-target', str(target_probability), scores_filepath, trials_filepath]
+  p = subprocess.Popen(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  output, err = p.communicate()
+  rc = p.returncode
+  if rc == 0:
+      output = output.decode("utf-8")
+      if log_directory is not None:
+          file = open(log_directory + '/dcf.log', 'w')
+          file.write(output)
+          file.close()
+      return float(output)
+  else:
+      exit(err)
