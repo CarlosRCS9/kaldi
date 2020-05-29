69a5629e64e6531f89db8fa84cc0efab3cdc3761
carlosrcs9 <carloscastillomvc@gmail.com>
2020-05-29 17:47:28 -0500

    TODO: rtmm_explicit_overlap.py

--- egs/dnn_tracking/v1/scripts/models.py
+++ egs/dnn_tracking/v1/scripts/models.py
@@ -5,6 +5,11 @@
  
 import numpy as np
 
+class Speaker:
+  def __init__(self, type, name):
+    self.type = type
+    self.name = name
+
 class Segment:
   def __init__(self, data):
     if isinstance(data, str):
@@ -15,16 +20,30 @@ class Segment:
       self.turn_onset            = np.float32(data[3])
       self.turn_duration         = np.float32(data[4])
       self.orthography_field     = data[5]
-      self.speaker_type          = data[6]
-      self.spekaer_name          = data[7]
+      self.speakers              = [Speaker(data[6], data[7])]
       self.confidence_score      = data[8]
       self.signal_lookahead_time = data[9]
+    elif isinstance(data, Segment):
+  def get_type(self):
+    return self.type
   def get_file_id(self):
     return self.file_id
-  def get_onset(self):
+  def get_channel_id(self):
+    return self.channel_id
+  def get_turn_onset(self):
     return self.turn_onset
-  def get_end(self):
+  def get_turn_duration(self):
+    return self.turn_duration
+  def get_turn_end(self):
     return self.turn_onset + self.turn_duration
+  def get_orthography_field(self):
+    return self.orthography_field
+  def get_speakers(self):
+    return self.speakers
+  def get_confidence_score(self):
+    return self.confidence_score
+  def get_signal_lookahead_time(self):
+    return self.signal_lookahead_time
   def is_within_timestamps(self, onset, end):
     return not (end <= self.get_onset() or self.get_end() <= onset)
   def __str__(self):
