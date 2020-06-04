33dd078d800edf8cd9196f80b516842185b2209a
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-04 14:23:35 -0500

    TODO: test sox_cut_and_stitch

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -24,6 +24,11 @@ def read_scp(scp_filepath):
   scps = sort_scps_by_file_id(scps)
   return scps
 
+def sox_cut_and_stitch(scp, timestamps_pairs, output_filepath):
+  trims = ['|sox ' + scp.get_filepath() + ' -t ' + scp.get_format() + ' - trim ' + str(onset) + ' ' + str(duration) for onset, duration in timestamps_pairs]
+  command = ['sox'] + trims + [output_filepath]
+  print(command)
+
 def main():
   args = get_args()
   stdin = get_stdin()
@@ -38,8 +43,10 @@ def main():
     file_segments = files_segments[file_id]
     speakers_segments = sort_segments_by_speakers(file_segments)
     single_speakers_segments = filter_by_speakers_length(speakers_segments, 1)
-    print(file_scp)
-    print(single_speakers_segments)
+    for speaker_name in single_speakers_segments.keys():
+      segments = single_speakers_segments[speaker_name]
+      timestamps_pairs = [(segment.get_turn_onset(), segment.get_turn_duration()) for segment in segments]
+      sox_cut_and_stitch(file_scp, timestamps_pairs, 'test')
 
 if __name__ == '__main__':
   main()
