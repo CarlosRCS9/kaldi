a3e2c725de1969358e8bce96e70b1c0252927732
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-05 10:17:33 -0500

    rttm_explicit_overlap.py updated

--- egs/dnn_tracking/v1/scripts/rttm_explicit_overlap.py
+++ egs/dnn_tracking/v1/scripts/rttm_explicit_overlap.py
@@ -7,13 +7,7 @@ import sys
 import functools
 import itertools
 
-from models import Segment
-
-def get_file_segments(acc, segment):
-  if segment.get_file_id() not in acc:
-    acc[segment.get_file_id()] = []
-  acc[segment.get_file_id()].append(segment)
-  return acc
+from models import Segment, sort_segments_by_file_id, get_segments_explicit_overlap
 
 def get_stdin():
   return sys.stdin
@@ -21,21 +15,12 @@ def get_stdin():
 def main():
   stdin = get_stdin()
   segments = [Segment(line) for line in stdin]
-  files_segments = functools.reduce(get_file_segments, segments, {})
-  new_segments = []
-  for file_id in sorted(files_segments.keys()):
-    file_segments = files_segments[file_id];
-    timestamps = sorted(set(itertools.chain.from_iterable([[segment.get_turn_onset(), segment.get_turn_end()] for segment in file_segments])))
-    timestamps_pairs = [(timestamps[i], timestamps[i + 1]) for i, _ in enumerate(timestamps[:-1])]
-    for onset, end in timestamps_pairs:
-      timestamps_segments = filter(lambda segment: segment.is_within_timestamps(onset, end), file_segments)
-      if len(timestamps_segments) > 0:
-        new_segment = Segment(timestamps_segments[0])
-        new_segment.add_speakers(list(itertools.chain.from_iterable([segment.get_speakers() for segment in timestamps_segments[1:]])))
-        new_segment.update_within_timestamps(onset, end)
-        new_segments.append(new_segment)
-  for segment in new_segments:
-    segment.print_rttm()
+  files_segments = sort_segments_by_file_id(segments)
+  files_segments = get_segments_explicit_overlap(files_segments)
+  for file_id in files_segments:
+    file_segments = files_segments[file_id]
+    for segment in file_segments:
+      segment.print_rttm()
 
 if __name__ == '__main__':
   main()
