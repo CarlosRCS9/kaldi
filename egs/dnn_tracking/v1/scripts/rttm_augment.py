703de1052c6b62b2844c32d67c5ccf6d3b086e0f
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-05 13:40:13 -0500

    TODO: test augment decimals

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -149,7 +149,7 @@ def main():
       left_duration = min_duration
       cut_durations = []
       while left_duration > 1.5:
-        cut_duration = math.sqrt(left_duration)
+        cut_duration = math.floor(math.sqrt(left_duration) * 1000.0) / 1000.0
         cut_durations.append(cut_duration)
         left_duration -= cut_duration
       cut_durations.append(left_duration)
