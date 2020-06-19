cb94cfb9896d3e2f54fd395769d5a6e385315951
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-19 00:09:40 -0500

    TODO: rttm_extract.py

--- egs/dnn_tracking/v1/scripts/rttm_cut.py
+++ egs/dnn_tracking/v1/scripts/rttm_cut.py
@@ -29,7 +29,14 @@ def main():
     file_segments = files_segments[file_id]
     for segment in file_segments:
       turn_onset = segment.get_turn_onset()
-      print(turn_onset)
+      while turn_onset < segment.get_turn_end():
+        turn_end = turn_onset + args.length
+        new_segment = Segment(segment)
+        new_segment.set_turn_onset(turn_onset)
+        new_segment.set_turn_end(turn_end)
+        if new_segment.get_turn_duration() >= args.min_length:
+          new_segment.print_rttm()
+        turn_onset = turn_end - args.overlap
 
 if __name__ == '__main__':
   main()
