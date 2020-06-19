45ed79c06de81f0e65146f3f138f6a21ea0edfc7
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-19 11:23:21 -0500

    channel_id in speaker

--- egs/dnn_tracking/v1/scripts/models.py
+++ egs/dnn_tracking/v1/scripts/models.py
@@ -84,19 +84,19 @@ class Scp:
 class Speaker:
   def __init__(self, data):
     if isinstance(data, list):
-      self.type = data[0]
-      self.name = data[1]
-      self.turn_onset = data[2]
-      self.turn_duration = data[3]
+      self.channel_id = data[0]
+      self.turn_onset = data[1]
+      self.turn_duration = data[2]
+      self.type = data[3]
+      self.name = data[4]
     elif isinstance(data, Speaker):
-      self.type = data.get_type()
-      self.name = data.get_name()
+      self.channel_id = data.get_channel_id()
       self.turn_onset = data.get_turn_onset()
       self.turn_duration = data.get_turn_duration()
-  def get_type(self):
-    return self.type
-  def get_name(self):
-    return self.name
+      self.type = data.get_type()
+      self.name = data.get_name()
+  def get_channel_id(self):
+    return self.channel_id
   def get_turn_onset(self):
     return self.turn_onset
   def set_turn_onset(self, turn_onset):
@@ -115,6 +115,10 @@ class Speaker:
     self.turn_duration = turn_end - self.get_turn_onset()
   def update_turn_end(self, turn_end):
     self.turn_duration = turn_end - self.get_turn_onset()
+  def get_type(self):
+    return self.type
+  def get_name(self):
+    return self.name
   def update_within_timestamps(self, onset, end):
     if onset < end:
       if self.get_turn_onset() < onset:
@@ -132,17 +136,15 @@ class Segment:
       data = data.split()
       self.type                  = data[0]
       self.file_id               = data[1]
-      self.channel_id            = data[2]
       self.turn_onset            = numpy.float32(data[3])
       self.turn_duration         = numpy.float32(data[4])
       self.orthography_field     = data[5]
-      self.speakers              = [Speaker([data[6], data[7], self.turn_onset, self.turn_duration])]
+      self.speakers              = [Speaker([data[2], self.turn_onset, self.turn_duration, data[6], data[7]])]
       self.confidence_score      = data[8]
       self.signal_lookahead_time = data[9]
     elif isinstance(data, Segment):
       self.type                  = data.get_type()
       self.file_id               = data.get_file_id()
-      self.channel_id            = data.get_channel_id()
       self.turn_onset            = data.get_turn_onset()
       self.turn_duration         = data.get_turn_duration()
       self.orthography_field     = data.get_orthography_field()
@@ -153,8 +155,6 @@ class Segment:
     return self.type
   def get_file_id(self):
     return self.file_id
-  def get_channel_id(self):
-    return self.channel_id
   def get_turn_onset(self):
     return self.turn_onset
   def set_turn_onset(self, turn_onset):
@@ -205,7 +205,7 @@ class Segment:
       print('WARNING: turn onset ' + str(onset) + ' is not less than end ' + str(end) + '.')
   def print_rttm(self):
     for speaker in self.get_speakers():
-      output = self.get_type() + ' ' + self.get_file_id() + ' ' + self.get_channel_id() + ' ' + \
+      output = self.get_type() + ' ' + self.get_file_id() + ' ' + speaker.get_channel_id() + ' ' + \
       str(round(speaker.get_turn_onset(), 3)) + ' ' + str(round(speaker.get_turn_duration(), 3)) + ' ' + \
       self.get_orthography_field() + ' ' + speaker.get_type() + ' ' + speaker.get_name() + \
       ' ' + self.get_confidence_score() + ' ' + self.get_signal_lookahead_time()
@@ -213,7 +213,7 @@ class Segment:
   def get_rttm(self):
     output = ''
     for speaker in self.get_speakers():
-      output += self.get_type() + ' ' + self.get_file_id() + ' ' + self.get_channel_id() + ' ' + \
+      output += self.get_type() + ' ' + self.get_file_id() + ' ' + speaker.get_channel_id() + ' ' + \
       str(round(speaker.get_turn_onset(), 3)) + ' ' + str(round(speaker.get_turn_duration(), 3)) + ' ' + \
       self.get_orthography_field() + ' ' + speaker.get_type() + ' ' + speaker.get_name() + \
       ' ' + self.get_confidence_score() + ' ' + self.get_signal_lookahead_time() + '\n'
