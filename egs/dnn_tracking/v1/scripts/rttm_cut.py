3397fbcece930f65ccc400782ee3ab2e98b70819
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-19 00:03:57 -0500

    TODO: rttm_extract.py

--- egs/dnn_tracking/v1/scripts/rttm_cut.py
+++ egs/dnn_tracking/v1/scripts/rttm_cut.py
@@ -10,8 +10,9 @@ from models import Segment, sort_segments_by_file_id
 
 def get_args():
   parser = argparse.ArgumentParser(description='')
-  parser.add_argument('segment_length', type=float, help='')
-  parser.add_argument('segment_overlap', type=float, help='')
+  parser.add_argument('length', type=float, help='')
+  parser.add_argument('overlap', type=float, help='')
+  parser.add_argument('--min-length', type=float, default=0.0, help='')
   args = parser.parse_args()
   return args
 
@@ -25,7 +26,10 @@ def main():
   segments = [Segment(line) for line in stdin]
   files_segments = sort_segments_by_file_id(segments)
   for file_id in sorted(files_segments.keys()):
-    print(file_id)
+    file_segments = files_segments[file_id]
+    for segment in file_segments:
+      turn_onset = segment.get_turn_onset()
+      print(turn_onset)
 
 if __name__ == '__main__':
   main()
