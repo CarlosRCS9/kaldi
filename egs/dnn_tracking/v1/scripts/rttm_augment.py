f5c115af1ab986982e12dbdc75f3793efb92b777
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-21 21:55:10 -0500

    TODO: rttm_augment.py

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -2,3 +2,20 @@
 
 # Copyright 2019 Carlos Castillo
 # Apache 2.0.
+
+def get_args():
+  parser = argparse.ArgumentParser(description='')
+  parser.add_argument('scp', type=str, help='')
+  parser.add_argument('output_folder', type=str, help='')
+  args = parser.parse_args()
+  return args
+
+def get_stdin():
+  return sys.stdin
+
+def main():
+  args = get_args()
+  stdin = get_stdin()
+
+if __name__ == '__main__':
+  main()
