7531df2e16310342056ef959e2e864f55909b357
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-21 22:59:11 -0500

    TODO: rttm_augment.py

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -27,11 +27,10 @@ def main():
 
   segments = [Segment(line) for line in stdin]
   files_segments = sort_segments_by_file_id(segments)
-  for file_id in sorted(files_segments.keys()):
+  for index, file_id in enumerate(sorted(files_segments.keys())):
+    print(index + 1, '/', len(file_segments.keys()), file_id, end = '\r')
     file_segments = files_segments[file_id]
     file_segments = get_segments_explicit_overlap(file_segments, 0.1)
-    for segment in file_segments:
-      print(segment.get_rttm(), end = '')
 
 if __name__ == '__main__':
   main()
