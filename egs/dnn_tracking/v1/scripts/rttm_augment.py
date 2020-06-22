7f497262750b187b81e23001ff77267c26fcbeb7
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-22 16:06:23 -0500

    TODO: generate overlap file

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -73,13 +73,14 @@ def main():
       for segment in list(filter(lambda segment: segment.get_turn_end() > min_duration, segments)):
         segment.set_turn_end(min_duration)
       segments = get_segments_explicit_overlap(segments)'''
+      print(duration)
       segments = list(itertools.chain(*[single_speakers_files[speaker_name]['segments'] for speaker_name in combination]))
       for segment in segments:
         print(segment.get_rttm(), end = '')
       segments = get_segments_explicit_overlap(segments)
       for segment in segments:
         print(segment.get_rttm(), end = '')
-      segments = list(filter(lambda segment: segment.get_turn_end() <= min_duration, segments))
+      segments = list(filter(lambda segment: segment.get_turn_end() <= duration, segments))
       print(len(segments))
       print('\n', filepath, duration, segments[-1].get_turn_end())
 
