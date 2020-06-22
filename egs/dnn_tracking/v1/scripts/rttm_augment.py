49ee698d8d4da2a46cb0c8e287204f12cb6ef961
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-22 10:40:22 -0500

    TODO: rttm_augment.py

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -34,12 +34,15 @@ def main():
   files_segments = sort_segments_by_file_id(segments)
   for index, file_id in enumerate(sorted(files_segments.keys())):
     print(index + 1, '/', len(files_segments.keys()), file_id, end = '\r')
+    file_scp = wav_scp[file_id]
     file_segments = files_segments[file_id]
     file_segments = get_segments_explicit_overlap(file_segments, 0.1)
     speakers_segments = sort_segments_by_speakers(file_segments)
     single_speakers_segments = filter_by_speakers_length(speakers_segments, 1)
+    single_speakers_files = {}
     for speaker_name in single_speakers_segments:
-      print(speaker_name)
+      filepath = output_folder + file_scp.get_file_id() + '_' + speaker_name + '.' + file_scp.get_format()
+      print(filepath)
 
 
 if __name__ == '__main__':
