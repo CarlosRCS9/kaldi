d8d6839a3f6f1a0d5fe5c866b48a17c7dbeba44b
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-22 22:11:22 -0500

    TODO: insert overlapped file

--- egs/dnn_tracking/v1/scripts/rttm_augment.py
+++ egs/dnn_tracking/v1/scripts/rttm_augment.py
@@ -89,8 +89,8 @@ def main():
     for _, combination_timestamps in combination_files.items():
       print(combination_timestamps)
       for turn_onset, turn_duration in combination_timestamps['timestamps_pairs']:
-       combination_timestamps.append({ 'filepath': combination_timestamps['filepath'], 'turn_onset': turn_onset, 'turn_duration': turn_duration, 'segments': combination_timestamps['segments'] })
-    for entry in combination_timestamps:
+       combinations_timestamps.append({ 'filepath': combination_timestamps['filepath'], 'turn_onset': turn_onset, 'turn_duration': turn_duration, 'segments': combination_timestamps['segments'] })
+    for entry in combinations_timestamps:
       print(entry)
 
 if __name__ == '__main__':
