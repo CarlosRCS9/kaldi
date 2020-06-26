f1ee9a88a46a4861cb0ddc601104266ac4cdf427
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-26 09:51:31 -0500

    TODO: extract features

--- egs/dnn_tracking/v1/ivector_extract.sh
+++ egs/dnn_tracking/v1/ivector_extract.sh
@@ -10,27 +10,30 @@ stage=0
 
 extractor_model=$1
 data_folder=$2
+output_folder=$data_folder/exp
+
+mkdir -p output_folder
 
 # Prepare features
 if [ $stage -le 1 ]; then
-  rm -rf $data_folder/make_mfcc
+  rm -rf $output_folder/make_mfcc
 
   steps/make_mfcc.sh --mfcc-config conf/mfcc_ivectors_dihard.conf --nj 40 \
     --cmd "$train_cmd" --write-utt2num-frames true --write-utt2dur true \
     $data_folder \
-    $data_folder/make_mfcc \
+    $output_folder/make_mfcc \
     $data_folder/mfcc
   utils/fix_data_dir.sh $data_folder
 fi
 
 # Extract i-vectors
 if [ $stage -le 2 ]; then
-  rm -rf $data_folder/make_ivectors
+  rm -rf $output_folder/make_ivectors
 
   diarization/extract_ivectors.sh --cmd "$train_cmd --mem 20G" \
     --nj 40 --window 1.5 --period 0.75 --apply-cmn false \
     --min-segment 0.5 \
     $extractor_model \
     $data_folder \
-    $data_folder/make_ivectors
+    $output_folder/make_ivectors
 fi
