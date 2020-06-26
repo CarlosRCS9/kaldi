f7a87547f2f4157d3d51a4fc95abc0fcb4ed626b
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-26 00:51:45 -0500

    TODO: generate extraction files

--- egs/dnn_tracking/v1/scripts/rttm_extract.py
+++ egs/dnn_tracking/v1/scripts/rttm_extract.py
@@ -17,6 +17,7 @@ def main():
   files_segments = sort_segments_by_file_id(segments)
 
   segments_data = ''
+  utt2spk_data = ''
   for index, file_id in enumerate(sorted(files_segments.keys())):
     print(index + 1, '/', len(files_segments.keys()), file_id, end = '\r')
     file_segments = get_segments_explicit_overlap(files_segments[file_id])
@@ -28,10 +29,12 @@ def main():
       segment.get_file_id() + ' ' + \
       str(round(segment.get_turn_onset(), 3)) + ' ' + \
       str(round(segment.get_turn_end(), 3)) + '\n'
+      utt2spk_data += utt + ' ' + segment.get_file_id() + '\n'
       count += 1
     break
 
-  print(segments_data, end = '')
+  #print(segments_data, end = '')
+  print(utt2spk_data, end = '')
 
 if __name__ == '__main__':
   main()
