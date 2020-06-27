7946a4bca5149e741308be15c67c8850661a2ca0
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-26 22:15:57 -0500

    TODO: create run.sh

--- egs/dnn_tracking/v1/run.sh
+++ egs/dnn_tracking/v1/run.sh
@@ -5,6 +5,13 @@
 data_folder=data/dihardii/
 output_folder=/export/b03/carlosc/data/2020/augmented/dihardii/
 
-for name in development evaluation; do
-  cat $data_folder$name/ref.rttm | python3 scripts/rttm_explicit_overlap.py > $output_folder$name/ref_explicit_overlap.rttm
-done
+stage=0
+
+# By default the RTTM file contains the speaker overlaps implicitly,
+# in the first stage we make these overlaps explicit.
+if [ $stage -le 0 ]; then
+  for name in development evaluation; do
+    cat $data_folder$name/ref.rttm | python3 scripts/rttm_explicit_overlap.py \
+    > $output_folder$name/ref_explicit_overlap.rttm
+  done
+fi
