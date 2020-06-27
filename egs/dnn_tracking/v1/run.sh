2cb321518adaf0721b6bf10a1075660d22a9d1b6
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-26 21:06:36 -0500

    TODO: create run.sh

--- egs/dnn_tracking/v1/run.sh
+++ egs/dnn_tracking/v1/run.sh
@@ -2,8 +2,10 @@
 # Copyright 2020 Carlos Castillo
 # Apache 2.0.
 
-ref_rtmm_filepath=data/dihardii/development/ref.rttm
+data_folder=data/dihardii/
+output_folder=/export/b03/carlosc/data/2020/augmented/dihardii/
 
 for name in development evaluation; do
-  echo $name
+  echo $data_folder$name/ref.rttm
+  echo $output_folder$name
 done
