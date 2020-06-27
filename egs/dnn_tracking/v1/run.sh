c927719ab79411c513ee76c69673c7baa8428981
carlosrcs9 <carloscastillomvc@gmail.com>
2020-06-26 22:06:18 -0500

    TODO: create run.sh

--- egs/dnn_tracking/v1/run.sh
+++ egs/dnn_tracking/v1/run.sh
@@ -6,6 +6,5 @@ data_folder=data/dihardii/
 output_folder=/export/b03/carlosc/data/2020/augmented/dihardii/
 
 for name in development evaluation; do
-  echo $data_folder$name/ref.rttm
-  echo $output_folder$name
+  cat $data_folder$name/ref.rttm | python3 scripts/rttm_explicit_overlap.py > $output_folder$name/ref_explicit_overlap.rttm
 done
