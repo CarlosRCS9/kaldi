565b026af736167d0a2635a74f00f4ca91cb33f7
CarlosRCS <carloscastillomvc@gmail.com>
2021-07-11 16:26:59 -0500

    rttm class

--- egs/crcs_utilities/v1/models.py
+++ egs/crcs_utilities/v1/models.py
@@ -3,10 +3,34 @@
 #
 # Apache 2.0
 
+import numpy as np
+
 class Rttm_line:
   def __init__ (self, data):
     if isinstance(data, str):
-      data = data.strip()
-      print(data)
-    else:
-      raise ValueError('invalid input data type')
+      data = data.strip().split()
+      if (len(data) != 10):
+        raise ValueError('invalid input string')
+      self.type  = data[0]
+      self.file  = data[1]
+      self.chnl  = data[2]
+      self.tbeg  = np.float32(data[3])
+      self.tdur  = np.float32(data[4])
+      self.ortho = data[5]
+      self.stype = data[6]
+      self.name  = data[7]
+      self.conf  = data[8]
+      self.slat  = data[9]
+      return
+    raise ValueError('invalid input data type')
+  def __str__ (self):
+    return f"{self.type} {self.file} {self.chnl} {self.tbeg:.2f} {self.tdur:.2f} {self.ortho} {self.stype} {self.name} {self.conf} {self.slat}"
+
+class Rttm:
+  def __init__ (self, data):
+    self.rttm_lines = [Rttm_line(entry) for entry in data]
+  def __str__ (self):
+    output = ''
+    for rttm_line in self.rttm_lines:
+      output += rttm_line.__str__() + '\n'
+    return output[:-1]
--- egs/crcs_utilities/v1/rttm_to_vad.py
+++ egs/crcs_utilities/v1/rttm_to_vad.py
@@ -3,7 +3,7 @@
 #
 # Apache 2.0
 
-from models import Rttm_line
+from models import Rttm
 import sys
 
 def get_stdin ():
@@ -11,8 +11,8 @@ def get_stdin ():
 
 def main ():
   stdin = get_stdin()
-  for line in stdin:
-    Rttm_line(line)
+  rttm = Rttm(stdin)
+  print(rttm)
 
 if __name__ == '__main__':
   main()
